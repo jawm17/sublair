40973828a03cf74c3ffabd5af1b5e191f0c8a530
Michael <jawm17@gmail.com>
2022-11-16 18:30:16 -0600

    ripples

---
+++ sublairApp/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import "./app.css";
+import $ from 'jquery';
+function App() {
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     window.test(400, 800);
+  //   }, 4000);
+  // },[])
+  return (
+    <div>
+
+    <div class="indexOuter">        
+      <div class="indexBackground" >
+           <div className="indeximage"></div>   
+     </div>
+   
+</div>
+  <div class="legalobligation"> 
+    <p><a class="policylink" href="https://alphamotif.net/policies/terms-of-service">TERMS OF SERVICE + </a> <a class="policylink" href="https://alphamotif.net/policies/privacy-policy">PRIVACY POLICY</a></p>
+  </div>
+</div>
+
+  );
+}
+
+export default App;
