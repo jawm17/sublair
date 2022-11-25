8733eb6e54f5a2f9b6cda51b7b02368b97b22fb8
Michael <jawm17@gmail.com>
2022-11-24 21:45:52 -0600

    paypal buttons

--- sublairApp/src/App.js
+++ sublairApp/src/App.js
@@ -1,26 +1,38 @@
 import { useEffect } from "react";
+import PaypalCheckoutButton from "./PaypalCheckoutButton";
 import "./app.css";
-import $ from 'jquery';
+
 function App() {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     window.test(400, 800);
-  //   }, 4000);
-  // },[])
+  const product = {
+    description: "zectangles",
+    price: 19
+  }
+
+  function rippleClick(e) {
+    // window.test(e.clientX + 75, e.clientY + 50);
+  }
+
   return (
     <div>
+      
+      <PaypalCheckoutButton product={product}/>
 
-    <div class="indexOuter">        
-      <div class="indexBackground" >
-           <div className="indeximage"></div>   
-     </div>
-   
-</div>
-  <div class="legalobligation"> 
-    <p><a class="policylink" href="https://alphamotif.net/policies/terms-of-service">TERMS OF SERVICE + </a> <a class="policylink" href="https://alphamotif.net/policies/privacy-policy">PRIVACY POLICY</a></p>
-  </div>
-</div>
 
+      {/* <div>
+        <div id="safeSpace" >
+        </div>
+        <div id="safeSpaceOuter" onClick={(e) => rippleClick(e)}>
+        </div>
+        <div class="indexOuter">
+          <div class="indexBackground">
+            <div className="indeximage"></div>
+          </div>
+        </div>
+        <div class="legalobligation">
+          <p><a class="policylink" href="https://alphamotif.net/policies/terms-of-service">TERMS OF SERVICE + </a> <a class="policylink" href="https://alphamotif.net/policies/privacy-policy">PRIVACY POLICY</a></p>
+        </div>
+      </div> */}
+    </div>
   );
 }
 
---
+++ sublairApp/src/PaypalCheckoutButton.js
@@ -0,0 +1,8 @@
+import { PayPalButtons } from "@paypal/react-paypal-js";
+const PaypalCheckoutButton = (props) => {
+    const { product } = props;
+
+    return <PayPalButtons />
+}
+
+export default PaypalCheckoutButton;
