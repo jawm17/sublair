12cdd03fc4fe65344c9a62d4f440b2984a9f55ca
Michael <jawm17@gmail.com>
2022-11-27 21:21:58 -0600

    new theme

--- sublairApp/src/App.js
+++ sublairApp/src/App.js
@@ -1,37 +1,204 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PaypalCheckoutButton from "./PaypalCheckoutButton";
+import upBtn from "./upBtn.png";
 import "./app.css";
 
 function App() {
+  let open = false;
   const product = {
     description: "zectangles",
     price: 19
   }
 
   function rippleClick(e) {
-    // window.test(e.clientX + 75, e.clientY + 50);
+    window.test(e.clientX + 75, e.clientY + 50);
+  }
+
+  // function openPage(state) {
+  //   document.getElementById("bg").style.transform = "scaleY(1)";
+  //   document.getElementById("mainFooter").style.top = "15px";
+  //   rotateBtn();
+  // }
+
+  function openPage() {
+    if (open === true) {
+      document.getElementById("mainFooter").style.top = "calc(100vh - 95px)";
+      document.getElementById("homeSlide").style.transform = "scaleY(0)";
+      document.getElementById("upBtn").style.transform = "rotate(0deg)";
+      document.getElementById("itemArea").style.opacity = "0%";
+    } else {
+      document.getElementById("homeSlide").style.transform = "scaleY(1)";
+      document.getElementById("mainFooter").style.top = "15px";
+      document.getElementById("upBtn").style.transform = "rotate(180deg)";
+
+      setTimeout(() => {
+        document.getElementById("itemArea").style.opacity = "100%";
+      }, 100);
+    }
+    open = !open;
   }
 
   return (
     <div>
-      
-      <PaypalCheckoutButton product={product}/>
+
+      {/* <PaypalCheckoutButton product={product}/> */}
 
 
-      {/* <div>
-        <div id="safeSpace" >
+
+      <div class="indexOuter">
+        <div className="indeximage"></div>
+      </div>
+
+
+
+      <div id="mainFooter">
+        <img src="https://sublair.com/images/LOGO.png" id="logo"></img>
+        <div id="footTitle">
+          SUBLAIR
         </div>
-        <div id="safeSpaceOuter" onClick={(e) => rippleClick(e)}>
-        </div>
-        <div class="indexOuter">
-          <div class="indexBackground">
-            <div className="indeximage"></div>
+        <img src={upBtn} id="upBtn" onClick={() => openPage()}></img>
+      </div>
+
+      <div id="homeSlide">
+
+
+        <div id="itemArea">
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
           </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/PADDEDVESTMAIN_2048x.png?v=1669329878" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/EHP02e1_2048x.png?v=1661040011" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src="https://cdn.shopify.com/s/files/1/1672/7095/products/DigitalPoltergeistMainE2_720x.png?v=1667081776" className="itemImage"></img>
+            <div className="itemInfo">
+              <div className="itemName">
+                clones t-shirt
+              </div>
+              <div className="itemPrice">
+                $125
+              </div>
+            </div>
+          </div>
+
+
         </div>
-        <div class="legalobligation">
-          <p><a class="policylink" href="https://alphamotif.net/policies/terms-of-service">TERMS OF SERVICE + </a> <a class="policylink" href="https://alphamotif.net/policies/privacy-policy">PRIVACY POLICY</a></p>
-        </div>
-      </div> */}
+      </div>
     </div>
   );
 }
--- sublairApp/src/PaypalCheckoutButton.js
+++ sublairApp/src/PaypalCheckoutButton.js
@@ -1,8 +1,65 @@
+import { useState } from "react";
 import { PayPalButtons } from "@paypal/react-paypal-js";
 const PaypalCheckoutButton = (props) => {
     const { product } = props;
 
-    return <PayPalButtons />
+    const [paidFor, setPaidFor] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleApprove = (orderId) => {
+        setPaidFor(true);
+    }
+
+    if (paidFor) {
+        // alert("success");
+    }
+
+    if (error) {
+        alert(error);
+    }
+
+    return <PayPalButtons
+        style={{
+            color: "silver",
+            height: 48,
+            tagline: false
+        }}
+        onClick={(data, actions) => {
+            const alreadyPurchased = false;
+
+            if(alreadyPurchased) {
+                setError("you already bought this item");
+                return actions.reject();
+            } else {
+                return actions.resolve();
+            }
+        }}
+        createOrder={(data, actions) => {
+            return actions.order.create({
+                purchase_units: [
+                    {
+                        description: product.description,
+                        amount: {
+                            value: product.price
+                        }
+                    }
+                ]
+            });
+        }}
+        onApprove={async (data, actions) => {
+            const order = await actions.order.capture();
+            console.log("order", order);
+
+            handleApprove(data.orderID);
+        }}
+        onCancel={() => {
+
+        }}
+        onError={(err) => {
+            setError(err);
+            console.log(err);
+        }}
+    />
 }
 
 export default PaypalCheckoutButton;
