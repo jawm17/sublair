618c25fef13200993d26a493e09d2401fb8e5226
Michael <jawm17@gmail.com>
2022-12-01 14:47:03 -0600

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ function Home() {
             document.getElementById("itemArea").style.opacity = "0%";
         } else {
             // page open
-            document.getElementById("mainFooter").style.bottom = "calc(100vh - (env(safe-area-inset-bottom) + 72px))";
+            document.getElementById("mainFooter").style.bottom = "calc(100vh - (env(safe-area-inset-bottom) + 200px))";
             document.getElementById("homeSlide").style.transform = "scaleY(1)";
             document.getElementById("upBtn").style.transform = "rotate(180deg)";
             setTimeout(() => {
