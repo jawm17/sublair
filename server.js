6af1d98fc583c1a347aa00b82868cb212a86082d
Michael <jawm17@gmail.com>
2022-12-27 00:42:26 -0600

    post

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 
-const db = "mongodb+srv://koikoi5:koikoi5@cluster0.lnkmi3d.mongodb.net/?retryWrites=true&w=majority";
-const db_dev = "mongodb+srv://koikoi5:koikoi5@cluster0.lnkmi3d.mongodb.net/?retryWrites=true&w=majority";
+const db = "mongodb://localhost:27017/sublair";
+const db_dev = "mongodb://localhost:27017/sublair";
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
